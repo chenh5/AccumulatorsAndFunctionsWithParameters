83af19d6abeb80a8a27bd126043bea561fad028b
chenh5 <chenh5@rose-hulman.edu>
2017-09-06 20:59:55 -0400

    m7,8,9

--- src/m9_summing_again.py
+++ src/m9_summing_again.py
@@ -4,8 +4,8 @@ in its simplest classic forms:
    SUMMING:       total = total + number
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Hui Chen.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -17,7 +17,7 @@ def main():
 def test_sum_powers():
     """ Tests the   sum_powers   function. """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement this function.
+    # DONE: 2. Implement this function.
     #   It TESTS the  sum_powers  function defined below.
     #   Include at least **   3   ** tests.
     #
@@ -28,6 +28,22 @@ def test_sum_powers():
     print('--------------------------------------------------')
     print('Testing the   sum_powers   function:')
     print('--------------------------------------------------')
+
+    # Test :
+    expected1 = 3.80826
+    answer1 = sum_powers(5, -0.3)
+    print('Test 1 expected:', expected1)
+    print('       actual:  ', answer1)
+
+    expected2 = 0
+    answer2 = sum_powers(0, 0.3)
+    print('Test 2 expected:', expected2)
+    print('       actual:  ', answer2)
+
+    expected3 = 144.45655
+    answer3 = sum_powers(100, 0.1)
+    print('Test 3 expected:', expected3)
+    print('       actual:  ', answer3)
 
 
 def sum_powers(n, p):
@@ -43,18 +59,24 @@ def sum_powers(n, p):
       -- sum_powers(100, 0.1) returns about 144.45655
     """
     # ------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     #
     #   No fair running the code of  sum_powers  to GENERATE
     #   test cases; that would defeat the purpose of TESTING!
     # ------------------------------------------------------------------
 
+    sum1 = 0
+    for k in range(n):
+        sum1 = sum1 + (k + 1)**p
+
+    return sum1
+
 
 def test_sum_powers_in_range():
     """ Tests the   sum_powers_in_range   function. """
     # ------------------------------------------------------------------
-    # TODO: 4. Implement this function.
+    # DONE: 4. Implement this function.
     #   It TESTS the  sum_powers_in_range  function defined below.
     #   Include at least **   3   ** tests.
     #
@@ -65,6 +87,22 @@ def test_sum_powers_in_range():
     print('--------------------------------------------------')
     print('Testing the   sum_powers_in_range   function:')
     print('--------------------------------------------------')
+
+    # Test :
+    expected1 = 1
+    answer1 = sum_powers_in_range(1, 1, 1)
+    print('Test 1 expected:', expected1)
+    print('       actual:  ', answer1)
+
+    expected2 = 5
+    answer2 = sum_powers_in_range(1, 2, 2)
+    print('Test 2 expected:', expected2)
+    print('       actual:  ', answer2)
+
+    expected3 = 142.384776
+    answer3 = sum_powers_in_range(3, 100, 0.1)
+    print('Test 3 expected:', expected3)
+    print('       actual:  ', answer3)
 
 
 def sum_powers_in_range(m, n, p):
@@ -80,7 +118,7 @@ def sum_powers_in_range(m, n, p):
       -- sum_powers_in_range(3, 100, 0.1) returns about 142.384776
     """
     # ------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # DONE: 5. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     #
     #   No fair running the code of  sum_powers_in_range  to GENERATE
@@ -89,7 +127,17 @@ def sum_powers_in_range(m, n, p):
     # COMMENT: Do you see how you could use   sum_powers_in_range
     #    to test  sum_powers   and (to a lesser extent) vice-versa?
     # ------------------------------------------------------------------
+    sum2 = 0
+    for k in range(n):
+        sum2 = sum2 + (k + 1)**p
 
+    sum3 = 0
+    for k in range(m-1):
+        sum3 = sum3 + (k + 1)**p
+
+    sum4 = sum2 - sum3
+
+    return sum4
 
 # ----------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
